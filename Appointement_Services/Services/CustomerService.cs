6c1c970ec0ce6b1f5a24c422bee6c0151ed6fd2a
Nick Stavrou <nickstavrou@gmail.com>
2020-10-12 18:51:25 +0300

    new service

---
+++ Appointement_Services/Services/CustomerService.cs
@@ -0,0 +1,47 @@
+﻿using Appointement_Services.Interfaces;
+using Appointment.Models;
+using Appointment_UI.AppContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointement_Services.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CustomerService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async ValueTask<Customer> CreateCustomerAsync(Customer customer)
+        {
+            _appDbContext.Add(customer);
+            await _appDbContext.SaveChangesAsync();
+            return customer;
+
+        }
+
+        public async ValueTask<IReadOnlyList<Customer>> GetAllCustomersAsync()
+        {
+            return await _appDbContext.Customers.ToListAsync();
+        }
+
+        public async ValueTask<Customer> RetrieveCustomerByIdAsync(Expression<Func<Customer, bool>> expression)
+        {
+            return await _appDbContext.Customers.FirstOrDefaultAsync(expression);
+        }
+
+        public async ValueTask<Customer> UpdateCustomerAsync(Customer customer)
+        {
+            _appDbContext.Customers.Update(customer);
+            await _appDbContext.SaveChangesAsync();
+            return customer;
+        }
+    }
+}
