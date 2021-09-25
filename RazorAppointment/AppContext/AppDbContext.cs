e9235e73fd11c3dccbd316ac56701ace130b3a84
nick stavrou <nickstavrou@gmail.com>
2021-09-25 17:29:12 +0300

    change from MVC to Razer Pages

---
+++ RazorAppointment/AppContext/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorAppointment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorAppointment.AppContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Appointment> Appointments { get; set; }
+
+    }
+}
